dd865613f98b8111226f45cb93315f38a79c76e6
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2024-02-15 07:49:26 -0500

    first commit

---
+++ Expense_Tracker.py
@@ -0,0 +1,18 @@
+
+def add_expense(expenses, amount, category):
+    expenses.append({'amount': amount, 'category': category})
+
+expenses = []
+
+
+
+
+
+
+
+def main():
+    print("Hello, World!")
+
+
+
+main()
