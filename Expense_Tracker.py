0c2a6129602d4b168151dd559779da8ccfa639c2
carlogyannuzzi <carlogyannuzzi@gmail.com>
2024-02-18 12:34:49 +0000

    completed the  main function, with prompts for user input to execute specific functions.

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -9,15 +9,45 @@ def print_expenses(expenses):
         pass
 
 def total_expenses(expenses):
-    pass
+    return sum(map(lambda expense: expense['amount'], expenses))
+
+def filter_expenses_by_category(expenses, category):
+    return filter(lambda expense: expense['category'] == category, expenses)
+    
 
 
 
 
 
 def main():
-    print("Hello, World!")
-
-
+    expenses = []
+    while True: 
+        print('\nExpense Tracker')
+        print('1. Add an expense')
+        print('2. List all expenses')
+        print('3. Show total expenses')
+        print('4. Filter expenses by category')
+        print('5. Exit')
+        choice = input('Enter your choice: ')
+        if choice == '1':
+            amount = input('Enter amount: ')
+            category = input('Enter category: ')
+            add_expense(expenses, amount, category)
+        elif choice == '2':
+            print('\nAll Expenses: ')
+            print_expenses(expenses)
+            print('\nTotal Expenses: ', total_expenses(expenses))
+            elif choice == '4':
+            category = input('Enter category to filter: ')
+            print(f"\nExpenses for {catergory}:")
+            expenses_from_category = filter_expenses_by_category(expenses, category)
+            print_expenses(expenses_from_category)
+            print('Exiting the program.')
+            break
 
 main()
+
+
+
+
+
